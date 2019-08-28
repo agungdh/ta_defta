b45fbdbf1c6d4d92c48c18b364f3e1fe8d5d874f
Agung Sapto Margono Dh <agungdh@live.com>
2019-08-28 21:36:43 +0700

    Add files via upload

--- defta.sql
+++ defta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2019 at 04:03 PM
+-- Generation Time: Aug 28, 2019 at 04:36 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -67,47 +67,281 @@ INSERT INTO `calon_dpd` (`id`, `nama`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kabupaten_kota`
+-- Table structure for table `kab`
 --
 
-CREATE TABLE `kabupaten_kota` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `kab` (
+  `id` varchar(4) NOT NULL,
   `dapil` int(11) NOT NULL,
-  `kabupaten_kota` varchar(191) NOT NULL
+  `nama_kab` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `kabupaten_kota`
+-- Dumping data for table `kab`
 --
 
-INSERT INTO `kabupaten_kota` (`id`, `dapil`, `kabupaten_kota`) VALUES
-(1, 1, 'LAMPUNG SELATAN'),
-(2, 2, 'BANDAR LAMPUNG'),
-(3, 3, 'METRO'),
-(4, 3, 'PESAWARAN'),
-(5, 3, 'PRINGSEWU'),
-(6, 4, 'TANGGAMUS'),
-(7, 4, 'LAMPUNG BARAT'),
-(8, 4, 'PESISIR BARAT'),
-(9, 5, 'LAMPUNG UTARA'),
-(10, 5, 'WAY KANAN'),
-(11, 6, 'TULANG BAWANG'),
-(12, 6, 'TULANG BAWANG BARAT'),
-(13, 6, 'MESUJI'),
-(14, 7, 'LAMPUNG TENGAH'),
-(15, 8, 'LAMPUNG TIMUR');
+INSERT INTO `kab` (`id`, `dapil`, `nama_kab`) VALUES
+('1801', 4, 'KABUPATEN LAMPUNG BARAT'),
+('1802', 4, 'KABUPATEN TANGGAMUS'),
+('1803', 1, 'LAMPUNG SELATAN'),
+('1804', 8, 'LAMPUNG TIMUR'),
+('1805', 7, 'LAMPUNG TENGAH'),
+('1806', 5, 'LAMPUNG UTARA'),
+('1807', 5, 'WAY KANAN'),
+('1808', 6, 'TULANGBAWANG'),
+('1809', 3, 'PESAWARAN'),
+('1810', 3, 'PRINGSEWU'),
+('1811', 6, 'MESUJI'),
+('1812', 6, 'TULANG BAWANG BARAT'),
+('1813', 4, 'PESISIR BARAT'),
+('1871', 2, 'BANDAR LAMPUNG'),
+('1872', 3, 'METRO');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kecamatan`
+-- Table structure for table `kec`
 --
 
-CREATE TABLE `kecamatan` (
-  `id` int(11) NOT NULL,
-  `id_kabupaten_kota` int(11) NOT NULL,
-  `kecamatan` varchar(191) NOT NULL
+CREATE TABLE `kec` (
+  `id` varchar(7) NOT NULL,
+  `kab_id` varchar(4) NOT NULL,
+  `nama_kec` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kec`
+--
+
+INSERT INTO `kec` (`id`, `kab_id`, `nama_kec`) VALUES
+('1801040', '1801', 'BALIK BUKIT'),
+('1801041', '1801', 'SUKAU'),
+('1801042', '1801', 'LUMBOK SEMINUNG'),
+('1801050', '1801', 'BELALAU'),
+('1801051', '1801', 'SEKINCAU'),
+('1801052', '1801', 'SUOH'),
+('1801053', '1801', 'BATU BRAK'),
+('1801054', '1801', 'PAGAR DEWA'),
+('1801055', '1801', 'BATU KETULIS'),
+('1801056', '1801', 'BANDAR NEGERI SUOH'),
+('1801060', '1801', 'SUMBER JAYA'),
+('1801061', '1801', 'WAY TENONG'),
+('1801062', '1801', 'GEDUNG SURIAN'),
+('1801063', '1801', 'KEBUN TEBU'),
+('1801064', '1801', 'AIR HITAM'),
+('1802010', '1802', 'WONOSOBO'),
+('1802011', '1802', 'SEMAKA'),
+('1802012', '1802', 'BANDAR NEGERI SEMUONG'),
+('1802020', '1802', 'KOTA AGUNG'),
+('1802021', '1802', 'PEMATANG SAWA'),
+('1802022', '1802', 'KOTA AGUNG TIMUR'),
+('1802023', '1802', 'KOTA AGUNG BARAT'),
+('1802030', '1802', 'PULAU PANGGUNG'),
+('1802031', '1802', 'ULUBELU'),
+('1802032', '1802', 'AIR NANINGAN'),
+('1802040', '1802', 'TALANG PADANG'),
+('1802041', '1802', 'SUMBEREJO'),
+('1802042', '1802', 'GISTING'),
+('1802043', '1802', 'GUNUNG ALIP'),
+('1802050', '1802', 'PUGUNG'),
+('1802101', '1802', 'BULOK'),
+('1802110', '1802', 'CUKUH BALAK'),
+('1802111', '1802', 'KELUMBAYAN'),
+('1802112', '1802', 'LIMAU'),
+('1802113', '1802', 'KELUMBAYAN BARAT'),
+('1803060', '1803', 'NATAR'),
+('1803070', '1803', 'JATI AGUNG'),
+('1803080', '1803', 'TANJUNG BINTANG'),
+('1803081', '1803', 'TANJUNG SARI'),
+('1803090', '1803', 'KATIBUNG'),
+('1803091', '1803', 'MERBAU MATARAM'),
+('1803092', '1803', 'WAY SULAN'),
+('1803100', '1803', 'SIDOMULYO'),
+('1803101', '1803', 'CANDIPURO'),
+('1803102', '1803', 'WAY PANJI'),
+('1803110', '1803', 'KALIANDA'),
+('1803111', '1803', 'RAJABASA'),
+('1803120', '1803', 'PALAS'),
+('1803121', '1803', 'SRAGI'),
+('1803130', '1803', 'PENENGAHAN'),
+('1803131', '1803', 'KETAPANG'),
+('1803132', '1803', 'BAKAUHENI'),
+('1804010', '1804', 'METRO KIBANG'),
+('1804020', '1804', 'BATANGHARI'),
+('1804030', '1804', 'SEKAMPUNG'),
+('1804040', '1804', 'MARGATIGA'),
+('1804050', '1804', 'SEKAMPUNG UDIK'),
+('1804060', '1804', 'JABUNG'),
+('1804061', '1804', 'PASIR SAKTI'),
+('1804062', '1804', 'WAWAY KARYA'),
+('1804063', '1804', 'MARGA SEKAMPUNG'),
+('1804070', '1804', 'LABUHAN MARINGGAI'),
+('1804071', '1804', 'MATARAM BARU'),
+('1804072', '1804', 'BANDAR SRIBAWONO'),
+('1804073', '1804', 'MELINTING'),
+('1804074', '1804', 'GUNUNG PELINDUNG'),
+('1804080', '1804', 'WAY JEPARA'),
+('1804081', '1804', 'BRAJA SLEBAH'),
+('1804082', '1804', 'LABUHAN RATU'),
+('1804090', '1804', 'SUKADANA'),
+('1804091', '1804', 'BUMI AGUNG'),
+('1804092', '1804', 'BATANGHARI NUBAN'),
+('1804100', '1804', 'PEKALONGAN'),
+('1804110', '1804', 'RAMAN UTARA'),
+('1804120', '1804', 'PURBOLINGGO'),
+('1804121', '1804', 'WAY BUNGUR'),
+('1805010', '1805', 'PADANG RATU'),
+('1805011', '1805', 'SELAGAI LINGGA'),
+('1805012', '1805', 'PUBIAN'),
+('1805013', '1805', 'ANAK TUHA'),
+('1805014', '1805', 'ANAK RATU AJI'),
+('1805020', '1805', 'KALIREJO'),
+('1805021', '1805', 'SENDANG AGUNG'),
+('1805030', '1805', 'BANGUNREJO'),
+('1805040', '1805', 'GUNUNG SUGIH'),
+('1805041', '1805', 'BEKRI'),
+('1805042', '1805', 'BUMI RATU NUBAN'),
+('1805050', '1805', 'TRIMURJO'),
+('1805060', '1805', 'PUNGGUR'),
+('1805061', '1805', 'KOTA GAJAH'),
+('1805070', '1805', 'SEPUTIH RAMAN'),
+('1805080', '1805', 'TERBANGGI BESAR'),
+('1805081', '1805', 'SEPUTIH AGUNG'),
+('1805082', '1805', 'WAY PENGUBUAN'),
+('1805090', '1805', 'TERUSAN NUNYAI'),
+('1805100', '1805', 'SEPUTIH MATARAM'),
+('1805101', '1805', 'BANDAR MATARAM'),
+('1805110', '1805', 'SEPUTIH BANYAK'),
+('1805111', '1805', 'WAY SEPUTIH'),
+('1805120', '1805', 'RUMBIA'),
+('1805121', '1805', 'BUMI NABUNG'),
+('1805122', '1805', 'PUTRA RUMBIA'),
+('1805130', '1805', 'SEPUTIH SURABAYA'),
+('1805131', '1805', 'BANDAR SURABAYA'),
+('1806010', '1806', 'BUKIT KEMUNING'),
+('1806011', '1806', 'ABUNG TINGGI'),
+('1806020', '1806', 'TANJUNG RAJA'),
+('1806030', '1806', 'ABUNG BARAT'),
+('1806031', '1806', 'ABUNG TENGAH'),
+('1806032', '1806', 'ABUNG  KUNANG'),
+('1806033', '1806', 'ABUNG PEKURUN'),
+('1806040', '1806', 'KOTABUMI'),
+('1806041', '1806', 'KOTABUMI UTARA'),
+('1806042', '1806', 'KOTABUMI SELATAN'),
+('1806050', '1806', 'ABUNG SELATAN'),
+('1806051', '1806', 'ABUNG SEMULI'),
+('1806052', '1806', 'BLAMBANGAN PAGAR'),
+('1806060', '1806', 'ABUNG TIMUR'),
+('1806061', '1806', 'ABUNG SURAKARTA'),
+('1806070', '1806', 'SUNGKAI SELATAN'),
+('1806071', '1806', 'MUARA SUNGKAI'),
+('1806072', '1806', 'BUNGA MAYANG'),
+('1806073', '1806', 'SUNGKAI  BARAT'),
+('1806074', '1806', 'SUNGKAI JAYA'),
+('1806080', '1806', 'SUNGKAI UTARA'),
+('1806081', '1806', 'HULUSUNGKAI'),
+('1806082', '1806', 'SUNGKAI TENGAH'),
+('1807010', '1807', 'BANJIT'),
+('1807020', '1807', 'BARADATU'),
+('1807021', '1807', 'GUNUNG LABUHAN'),
+('1807030', '1807', 'KASUI'),
+('1807031', '1807', 'REBANG TANGKAS'),
+('1807040', '1807', 'BLAMBANGAN UMPU'),
+('1807041', '1807', 'WAY TUBA'),
+('1807042', '1807', 'NEGERI AGUNG'),
+('1807050', '1807', 'BAHUGA'),
+('1807051', '1807', 'BUAY  BAHUGA'),
+('1807052', '1807', 'BUMI AGUNG'),
+('1807060', '1807', 'PAKUAN RATU'),
+('1807061', '1807', 'NEGARA BATIN'),
+('1807062', '1807', 'NEGERI BESAR'),
+('1808030', '1808', 'BANJAR AGUNG'),
+('1808031', '1808', 'BANJAR MARGO'),
+('1808032', '1808', 'BANJAR BARU'),
+('1808040', '1808', 'GEDUNG AJI'),
+('1808041', '1808', 'PENAWAR AJI'),
+('1808042', '1808', 'MERAKSA AJI'),
+('1808050', '1808', 'MENGGALA'),
+('1808051', '1808', 'PENAWAR TAMA'),
+('1808052', '1808', 'RAWAJITU SELATAN'),
+('1808053', '1808', 'GEDUNG MENENG'),
+('1808054', '1808', 'RAWAJITU TIMUR'),
+('1808055', '1808', 'RAWA PITU'),
+('1808056', '1808', 'GEDUNG AJI BARU'),
+('1808057', '1808', 'DENTE TELADAS'),
+('1808058', '1808', 'MENGGALA TIMUR'),
+('1809010', '1809', 'PUNDUH PIDADA'),
+('1809011', '1809', 'MARGA PUNDUH'),
+('1809020', '1809', 'PADANG CERMIN'),
+('1809021', '1809', 'TELUK PANDAN'),
+('1809022', '1809', 'WAY RATAI'),
+('1809030', '1809', 'KEDONDONG'),
+('1809031', '1809', 'WAY KHILAU'),
+('1809040', '1809', 'WAY LIMA'),
+('1809050', '1809', 'GEDUNG TATAAN'),
+('1809060', '1809', 'NEGERI KATON'),
+('1809070', '1809', 'TEGINENENG'),
+('1810010', '1810', 'PARDASUKA'),
+('1810020', '1810', 'AMBARAWA'),
+('1810030', '1810', 'PAGELARAN'),
+('1810031', '1810', 'PAGELARAN UTARA'),
+('1810040', '1810', 'PRINGSEWU'),
+('1810050', '1810', 'GADING REJO'),
+('1810060', '1810', 'SUKOHARJO'),
+('1810070', '1810', 'BANYUMAS'),
+('1810080', '1810', 'ADI LUWIH'),
+('1811010', '1811', 'WAY SERDANG'),
+('1811020', '1811', 'SIMPANG PEMATANG'),
+('1811030', '1811', 'PANCA JAYA'),
+('1811040', '1811', 'TANJUNG RAYA'),
+('1811050', '1811', 'MESUJI'),
+('1811060', '1811', 'MESUJI TIMUR'),
+('1811070', '1811', 'RAWAJITU UTARA'),
+('1812010', '1812', 'TULANG BAWANG UDIK'),
+('1812020', '1812', 'TUMI JAJAR'),
+('1812030', '1812', 'TULANG BAWANG TENGAH'),
+('1812040', '1812', 'PAGAR DEWA'),
+('1812050', '1812', 'LAMBU KIBANG'),
+('1812060', '1812', 'GUNUNG TERANG'),
+('1812061', '1812', 'BATU PUTIH'),
+('1812070', '1812', 'GUNUNG AGUNG'),
+('1812080', '1812', 'WAY KENANGA'),
+('1813010', '1813', 'LEMONG'),
+('1813020', '1813', 'PESISIR UTARA'),
+('1813030', '1813', 'PULAU PISANG'),
+('1813040', '1813', 'KARYA PENGGAWA'),
+('1813050', '1813', 'WAY KRUI'),
+('1813060', '1813', 'PESISIR TENGAH'),
+('1813070', '1813', 'KRUI SELATAN'),
+('1813080', '1813', 'PESISIR SELATAN'),
+('1813090', '1813', 'NGAMBUR'),
+('1813100', '1813', 'BENGKUNAT'),
+('1813110', '1813', 'BENGKUNAT BELIMBING'),
+('1871010', '1871', 'TELUK BETUNG BARAT'),
+('1871011', '1871', 'TELUKBETUNG TIMUR'),
+('1871020', '1871', 'TELUK BETUNG SELATAN'),
+('1871021', '1871', 'BUMI WARAS'),
+('1871030', '1871', 'PANJANG'),
+('1871040', '1871', 'TANJUNG KARANG TIMUR'),
+('1871041', '1871', 'KEDAMAIAN'),
+('1871050', '1871', 'TELUK BETUNG UTARA'),
+('1871060', '1871', 'TANJUNG KARANG PUSAT'),
+('1871061', '1871', 'ENGGAL'),
+('1871070', '1871', 'TANJUNG KARANG BARAT'),
+('1871071', '1871', 'KEMILING'),
+('1871072', '1871', 'LANGKAPURA'),
+('1871080', '1871', 'KEDATON'),
+('1871081', '1871', 'RAJABASA'),
+('1871082', '1871', 'TANJUNG SENANG'),
+('1871083', '1871', 'LABUHAN RATU'),
+('1871090', '1871', 'SUKARAME'),
+('1871091', '1871', 'SUKABUMI'),
+('1871092', '1871', 'WAY HALIM'),
+('1872011', '1872', 'METRO SELATAN'),
+('1872012', '1872', 'METRO BARAT'),
+('1872021', '1872', 'METRO TIMUR'),
+('1872022', '1872', 'METRO PUSAT'),
+('1872023', '1872', 'METRO UTARA');
 
 -- --------------------------------------------------------
 
@@ -142,41 +376,6 @@ INSERT INTO `partai` (`id`, `nama`) VALUES
 (15, 'PARTAI BULAN BINTANG'),
 (16, 'PKPI');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `periode`
---
-
-CREATE TABLE `periode` (
-  `id` int(11) NOT NULL,
-  `periode` varchar(191) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `peserta_dpd`
---
-
-CREATE TABLE `peserta_dpd` (
-  `id` int(11) NOT NULL,
-  `id_periode` int(11) NOT NULL,
-  `id_calon_dpd` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `peserta_partai`
---
-
-CREATE TABLE `peserta_partai` (
-  `id` int(11) NOT NULL,
-  `id_periode` int(11) NOT NULL,
-  `id_partai` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 --
 -- Indexes for dumped tables
 --
@@ -188,45 +387,23 @@ ALTER TABLE `calon_dpd`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kabupaten_kota`
+-- Indexes for table `kab`
 --
-ALTER TABLE `kabupaten_kota`
+ALTER TABLE `kab`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kecamatan`
+-- Indexes for table `kec`
 --
-ALTER TABLE `kecamatan`
+ALTER TABLE `kec`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_kabupaten_kota` (`id_kabupaten_kota`);
+  ADD KEY `kab_id` (`kab_id`);
 
 --
 -- Indexes for table `partai`
 --
 ALTER TABLE `partai`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `periode`
---
-ALTER TABLE `periode`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `peserta_dpd`
---
-ALTER TABLE `peserta_dpd`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_calon_dpd` (`id_calon_dpd`),
-  ADD KEY `id_periode` (`id_periode`);
-
---
--- Indexes for table `peserta_partai`
---
-ALTER TABLE `peserta_partai`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_partai` (`id_partai`),
-  ADD KEY `id_periode` (`id_periode`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -239,64 +416,20 @@ ALTER TABLE `calon_dpd`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT for table `kabupaten_kota`
---
-ALTER TABLE `kabupaten_kota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `kecamatan`
---
-ALTER TABLE `kecamatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `partai`
 --
 ALTER TABLE `partai`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT for table `periode`
---
-ALTER TABLE `periode`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `peserta_dpd`
---
-ALTER TABLE `peserta_dpd`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `peserta_partai`
---
-ALTER TABLE `peserta_partai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `kecamatan`
+-- Constraints for table `kec`
 --
-ALTER TABLE `kecamatan`
-  ADD CONSTRAINT `kecamatan_ibfk_1` FOREIGN KEY (`id_kabupaten_kota`) REFERENCES `kabupaten_kota` (`id`);
-
---
--- Constraints for table `peserta_dpd`
---
-ALTER TABLE `peserta_dpd`
-  ADD CONSTRAINT `peserta_dpd_ibfk_1` FOREIGN KEY (`id_calon_dpd`) REFERENCES `calon_dpd` (`id`),
-  ADD CONSTRAINT `peserta_dpd_ibfk_2` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id`);
-
---
--- Constraints for table `peserta_partai`
---
-ALTER TABLE `peserta_partai`
-  ADD CONSTRAINT `peserta_partai_ibfk_1` FOREIGN KEY (`id_partai`) REFERENCES `partai` (`id`),
-  ADD CONSTRAINT `peserta_partai_ibfk_2` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id`);
+ALTER TABLE `kec`
+  ADD CONSTRAINT `kec_ibfk_1` FOREIGN KEY (`kab_id`) REFERENCES `kab` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
